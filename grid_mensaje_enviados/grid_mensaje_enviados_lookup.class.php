d1129e18fb176ed421357dc77e1bd5de6f2219fd
eepc20 <eepc2024@gmail.com>
2018-11-30 10:19:14 -0600

    Carga de la funcionalidad de notificaciones internas del sistema

---
+++ grid_mensaje_enviados/grid_mensaje_enviados_lookup.class.php
@@ -0,0 +1,41 @@
+<?php
+class grid_mensaje_enviados_lookup
+{
+//  
+   function lookup_enviado_a(&$conteudo , $enviado_a) 
+   {   
+      static $save_conteudo = "" ; 
+      static $save_conteudo1 = "" ; 
+      $tst_cache = $enviado_a; 
+      if ($tst_cache === $save_conteudo && $conteudo != "&nbsp;") 
+      { 
+          $conteudo = $save_conteudo1 ; 
+          return ; 
+      } 
+      $save_conteudo = $tst_cache ; 
+      $nm_comando = "select usuario_nombre from usuarios where usuario = '" . substr($this->Db->qstr($enviado_a), 1 , -1) . "' order by usuario_nombre" ; 
+      $conteudo = "" ; 
+      $_SESSION['scriptcase']['sc_sql_ult_comando'] = $nm_comando; 
+      $_SESSION['scriptcase']['sc_sql_ult_conexao'] = ''; 
+      if ($rx = $this->Db->Execute($nm_comando)) 
+      { 
+          if (isset($rx->fields[0]))  
+          { 
+              $conteudo = trim($rx->fields[0]); 
+          } 
+          $save_conteudo1 = $conteudo ; 
+          $rx->Close(); 
+      } 
+      elseif ($GLOBALS["NM_ERRO_IBASE"] != 1)  
+      { 
+          $this->Erro->mensagem(__FILE__, __LINE__, "banco", $this->Ini->Nm_lang['lang_errm_dber'], $this->Db->ErrorMsg()); 
+          exit; 
+      } 
+      if ($conteudo === "") 
+      { 
+          $conteudo = "&nbsp;";
+          $save_conteudo1 = $conteudo ; 
+      } 
+   }  
+}
+?>
